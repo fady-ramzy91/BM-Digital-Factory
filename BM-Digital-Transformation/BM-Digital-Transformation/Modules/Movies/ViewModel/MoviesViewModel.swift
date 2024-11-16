4cb5105fe62723229bf7aff709af3d94a197dc34
Fady Ramzy <fady.ramzy@areebgroup.com>
2024-11-16 15:37:54 +0200

    ADD: Added MoviesViewModel which will be responsible  for the presentation logic related to the movies view

---
+++ BM-Digital-Transformation/BM-Digital-Transformation/Modules/Movies/ViewModel/MoviesViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  MoviesViewModel.swift
+//  BM-Digital-Transformation
+//
+//  Created by Fady Ramzy on 15/11/2024.
+//
+
+final class MoviesViewModel {
+  // MARK: - Properties
+  private let useCase: MoviesUseCaseProtocol
+  var movies: [MovieUIModel] = []
+  
+  // MARK: - Init Methods
+  init(useCase: MoviesUseCaseProtocol) {
+    self.useCase = useCase
+  }
+  
+  func startFetchingMovies(with moviesType: MoviesType) async throws {
+    movies = try await useCase.startFetchingMovies(with: moviesType)
+  }
+}
